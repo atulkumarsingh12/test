54f35e83997035ae645489601ee0e532cfc6406f
atul <test@cedcoss.com>
2017-02-15 14:23:31 +0530

    row div is commit

--- index.php
+++ index.php
@@ -10,12 +10,31 @@
 <body>
     <div id="wrapper">Wrapper
     		<div id="main">
-    			<div class="container"></div>
+    			<div class="container">
+    				<h1>My first Bootstrap Page.</h1>
+					<p>This is some text.</p>
+					<p>The .container-fluid class provides a full width container, spanning the entire width of the viewport.</p>
+				</div>
 
     		</div>
     		<div id="footer">Footer</div>
 
     </div>
+    <div class="row">
+		<div class="col-md-4" style="background-color:lavender;">
+				<p>For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.</p>
+		</div>
+		<div class="col-md-4" style="background-color:lavenderblush;">
+			<p>For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.</p>
+
+	   	</div>
+		<div class="col-md-4" style="background-color:lavender;">
+			<p>For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.</p>
+	  </div>
+
+
+</div>
+
 
 </body>
 </html>
